34ef6965a6abab1d67bb022a62ce241b6573eca8
swapyshelke <swapyshelke02gmail.com>
2025-09-12 00:37:16 +0530

    frontend completed

--- react_reduc_cart/src/components/Cards.jsx
+++ react_reduc_cart/src/components/Cards.jsx
@@ -5,6 +5,8 @@ import Card from 'react-bootstrap/Card';
 
 import CardsData from "./CardsData"
 
+import "./style.css"
+
 const Cards = () => {
 
     const [data, setData] = useState(CardsData);
@@ -15,20 +17,21 @@ const Cards = () => {
         <div className="container mt-3">
             <h2 className="text-center">Add to Card Items...</h2>
 
-            <div className="row">
+            <div className="row d-flex justify-content-center align-items-center">
                 {
                     data.map((element, id) =>{
                         return (
                             <>
-                             <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={element.imgdata} />
+                             <Card style={{ width: '22rem', border:"none" }} className='mx-2 mt-4  card_style'>
+      <Card.Img variant="top" src={element.imgdata} style={{height:"16rem"}} className='mt-3' />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{element.rname}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          Price: {element.price} $
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <div className="button_div d-flex justify-content-center">
+        <Button variant="primary"  className='col-lg-12'>Add to cart</Button>
+        </div>
       </Card.Body>
     </Card>
                             </>
